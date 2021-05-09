6b20f828669ed1260b85ccf518fe89d5704b58e1
Daniel Rodas <drodas6232@gmail.com>
2021-05-08 23:27:55 -0600

    16th Commit (Still debuggin captcha)

--- public/script.js
+++ public/script.js
@@ -3,20 +3,26 @@ const openNavBtn = document.querySelector("#open-nav-btn");
 const closeNavBtn = document.querySelector("#close-nav-btn");
 const navMenuLinks = document.querySelector("#nav-links-col");
 const navContainer = document.querySelector("#nav-container");
+const captcha = document.querySelector('.rc-anchor-normal {
+    ')
 
-//Event Listeners
-openNavBtn.addEventListener("click", () => {
-  if (navMenuLinks.classList.contains("translate-x-full")) {
-    openNavBtn.classList.add("hidden");
-    closeNavBtn.classList.remove("hidden");
-    navMenuLinks.classList.remove("translate-x-full");
-    navContainer.classList.remove("border-b");
-  }
-});
+    // Navigation Bar
+    //Event Listeners
+    openNavBtn.addEventListener("click", () => {
+      if (navMenuLinks.classList.contains("translate-x-full")) {
+        openNavBtn.classList.add("hidden");
+        closeNavBtn.classList.remove("hidden");
+        navMenuLinks.classList.remove("translate-x-full");
+        navContainer.classList.remove("border-b");
+      }
+    });
 
-closeNavBtn.addEventListener("click", () => {
-  closeNavBtn.classList.add("hidden");
-  openNavBtn.classList.remove("hidden");
-  navMenuLinks.classList.add("translate-x-full");
-  navContainer.classList.add("border-b");
-});
+    closeNavBtn.addEventListener("click", () => {
+      closeNavBtn.classList.add("hidden");
+      openNavBtn.classList.remove("hidden");
+      navMenuLinks.classList.add("translate-x-full");
+      navContainer.classList.add("border-b");
+    });
+
+    //Captcha resize
+    captcha.style.width = "275px";
